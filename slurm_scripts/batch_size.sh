dd31fdbbcf5ef5ea3fb0b679ee31431f3d3e3f41
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-12-08 11:31:58 +0100

    fix

--- slurm_scripts/batch_size.sh
+++ slurm_scripts/batch_size.sh
@@ -4,5 +4,7 @@
 #SBATCH --error=log/BS/%j/errors.err # fichier d’erreur (%j = job ID)
 #SBATCH --ntasks=1 #reserver 10 taches (ou processus)
 #SBATCH --time=168:00:00 # temps d'allocation
+
+export GUROBI_HOME="/Brain/private/j20morli/jobsSlurm/gurobi.llc"
 source .venvbatch/bin/activate
 srun python3 evaluate_batch_size.py
