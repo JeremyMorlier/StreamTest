64cfbcaf4b09d629e7f596cae8a7573f68e71b87
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-12-08 09:57:45 +0100

    slurm scripts

--- slurm_scripts/batch_size.sh
+++ slurm_scripts/batch_size.sh
@@ -2,9 +2,7 @@
 #SBATCH --job-name=BS # nom du job
 #SBATCH --output=log/BS/%j/logs.out # fichier de sortie (%j = job ID)
 #SBATCH --error=log/BS/%j/errors.err # fichier d’erreur (%j = job ID)
-#SBATCH --nodes=1 # reserver 1 nœud
 #SBATCH --ntasks=1 #reserver 10 taches (ou processus)
 #SBATCH --time=168:00:00 # temps d'allocation
-
 source .venvbatch/bin/activate
 srun python3 evaluate_batch_size.py
