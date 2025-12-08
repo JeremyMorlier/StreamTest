192fddb54a45f73e3e312ad28c0d1b457c73fb6c
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-12-08 09:47:38 +0100

    fix

--- slurm_scripts/batch_size.sh
+++ slurm_scripts/batch_size.sh
@@ -4,8 +4,6 @@
 #SBATCH --error=log/BS/%j/errors.err # fichier d’erreur (%j = job ID)
 #SBATCH --nodes=1 # reserver 1 nœud
 #SBATCH --ntasks=1 #reserver 10 taches (ou processus)
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=0 # reserve toute la mémoire
 #SBATCH --time=168:00:00 # temps d'allocation
 
 source .venvbatch/bin/activate
