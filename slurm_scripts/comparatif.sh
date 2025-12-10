fd95ac7aec0a8ed4d014bf417c6d7d6915eaa139
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2025-12-10 17:05:56 +0100

    update

--- slurm_scripts/comparatif.sh
+++ slurm_scripts/comparatif.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=SA # nom du job
-#SBATCH --output=log/SA/%j/logs.out # fichier de sortie (%j = job ID)
-#SBATCH --error=log/SA/%j/errors.err # fichier d’erreur (%j = job ID)
+#SBATCH --job-name=CA # nom du job
+#SBATCH --output=log/CA/%j/logs.out # fichier de sortie (%j = job ID)
+#SBATCH --error=log/CA/%j/errors.err # fichier d’erreur (%j = job ID)
 #SBATCH --nodes=1 # reserver 1 nœud
 #SBATCH --ntasks=1 #reserver 10 taches (ou processus)
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=0 # reserve toute la mémoire
 #SBATCH --time=168:00:00 # temps d'allocation
 #SBATCH --nodelist=sl-mee-br-119
 #SBATCH -p AAI
+
+export GRB_LICENSE_FILE=../jobsSlurm/gurobi.llc
 
 source .venvbatch/bin/activate
 srun python3 comparatif.py --processes 8 --output_path results/full_resnet/
